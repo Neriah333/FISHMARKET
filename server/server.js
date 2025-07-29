b30234a8d577969a85049deb5fa881807e124340
Neriah333 <pheobenyawanda@gmail.com>
2025-07-29 11:05:15 +0300

    fix: add root route for Render

--- server/server.js
+++ server/server.js
@@ -16,5 +16,9 @@ app.use("/api/sales", require("./routes/fishSaleRoutes"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
 app.use("/api/contact", require("./routes/contact"));
 
+app.get("/", (req, res) => {
+  res.send("🎣 Fish Market API is running!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
